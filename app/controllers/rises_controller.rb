7c6d5b914ea17c3631e795b4a7c09ebf837c2d7b
Benson Kalahar <bensonk42@gmail.com>
2012-02-16 00:32:32 -0800

    Testing for a heroku fix

--- app/controllers/rises_controller.rb
+++ app/controllers/rises_controller.rb
@@ -15,7 +15,11 @@ class RisesController < ApplicationController
   def show
     @rise = Rise.find(params[:id])
     max_height = @rise.heights.max
-    @dough_heights = @rise.heights.collect { |h| max_height - h + @rise.base_height }
+    logger.info "Type of max_height: #{max_height.class.to_s}"
+    logger.info "Type of rise.base_height: #{@rise.base_height.class.to_s}"
+    logger.info "Type of h: #{@rise.heights[0].class.to_s}"
+
+    @dough_heights = @rise.heights.collect { |h| max_height - h + @rise.base_height.to_f }
 
     respond_to do |format|
       format.html # show.html.erb
